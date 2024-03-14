1cb954ce00849e7b969ca777a225f1006451ff92
janowol <janos.wolosz@gmail.com>
2024-03-14 10:37:38 +0100

    Add scripts + cmake

---
+++ source/scripts/build/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+if [[ $1 != "debug" ]] && [[ $1 != "release" ]]; then
+    echo "First command line argument should be debug or release"
+    exit 1
+fi	
+CMAKE_BUILD_TYPE=$1
+SCRIPT_FOLDER=$(dirname "${0}") 
+WORKSPACE_FOLDER=$(readlink -e "${SCRIPT_FOLDER}/../../")
+BUILD_FOLDER="${WORKSPACE_FOLDER}/build/${CMAKE_BUILD_TYPE}"
+CMAKE_CXX_COMPILER=g++
+CMAKE_MAKE_PROGRAM=ninja
