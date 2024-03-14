3ce57eecfd48e57e74a906fcb048dc8becbe1655
janowol <janos.wolosz@gmail.com>
2024-03-14 11:48:10 +0100

    Add singleton test

--- source/test/singleton/TestSingleton.cpp
+++ source/test/singleton/TestSingleton.cpp
@@ -1,5 +1,18 @@
 #include <gmock/gmock.h>
 
+#include <loki/Singleton.h>
+#include <iostream>
+
+class A
+{
+public:
+  void print() const { std::cout << "A\n"; }
+};
+
+typedef Loki::SingletonHolder<A, Loki::CreateUsingNew> SingleA;
+
 TEST(Singleton, TestSingleton)
 {
+  const auto& a = SingleA::Instance();
+  a.print();
 }
