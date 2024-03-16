33c675595dfe9e8cc8f3aec7eddcaeab2a8ddf37
janowol <janos.wolosz@gmail.com>
2024-03-16 07:18:03 +0100

    Test smart_ptr deepcopy

---
+++ source/test/smart_ptr/TestSmartPtr.cpp
@@ -0,0 +1,30 @@
+#include <gmock/gmock.h>
+
+#include <loki/SmartPtr.h>
+#include <iostream>
+#include <sstream>
+
+struct TDeepCopy
+{
+  int i;
+  std::string s;
+
+  TDeepCopy* Clone() { return new TDeepCopy{i, s}; }
+};
+
+TEST(SmartPtr, TestDeepCopy)
+{
+  auto* ptr = new TDeepCopy{1, "test"};
+  Loki::SmartPtr<TDeepCopy, Loki::DeepCopy> tPtr1(ptr);
+  Loki::SmartPtr<TDeepCopy, Loki::DeepCopy> tPtr2 = tPtr1;
+  tPtr1->i = 1;
+  tPtr1->s = "test1";
+  tPtr2->i = 2;
+  tPtr2->s = "test2";
+  EXPECT_EQ(ptr->i, 1);
+  EXPECT_EQ(ptr->s, "test1");
+  EXPECT_EQ(tPtr1->i, 1);
+  EXPECT_EQ(tPtr1->s, "test1");
+  EXPECT_EQ(tPtr2->i, 2);
+  EXPECT_EQ(tPtr2->s, "test2");
+}
