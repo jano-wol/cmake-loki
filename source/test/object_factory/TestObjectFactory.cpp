1ebf9417aecf7bee5b5fa9ff64d37bfabb8bc578
janowol <janos.wolosz@gmail.com>
2024-03-17 08:20:35 +0100

    Add test

--- source/test/object_factory/TestObjectFactory.cpp
+++ source/test/object_factory/TestObjectFactory.cpp
@@ -7,63 +7,58 @@
 class Shape
 {
 public:
-  virtual int calculate() = 0;
   virtual ~Shape(){};
+  virtual int calculate() const = 0;
+
+  void setPos(std::pair<int, int> pos_) { pos = std::move(pos_); }
+
+protected:
+  std::pair<int, int> pos;
 };
 
 class Polygon : public Shape
 {
 public:
-  Polygon(int i_) : i(i_) {}
   ~Polygon(){};
-
-  int calculate() override { return i + 3; }
-
-private:
-  int i;
+  int calculate() const override { return pos.first + pos.second + 3; }
 };
 
 class Line : public Shape
 {
 public:
-  Line(int i_) : i(i_) {}
   ~Line(){};
-
-  int calculate() override { return i + 2; }
-
-private:
-  int i;
+  int calculate() const override { return pos.first + pos.second + 2; }
 };
 
 class Circle : public Shape
 {
 public:
-  Circle(int i_) : i(i_) {}
   ~Circle(){};
-
-  int calculate() override { return i + 360; }
-
-private:
-  int i;
+  int calculate() const override { return pos.first + pos.second + 360; }
 };
 
-Polygon* createPolygon() { return new Polygon(0); }
-Line* createLine() { return new Line(0); }
-Circle* createCircle() { return new Circle(0); }
+Polygon* createPolygon() { return new Polygon; }
+Line* createLine() { return new Line; }
+Circle* createCircle() { return new Circle; }
 
 TEST(ObjectFactory, TestObjectFactory)
 {
-  typedef Loki::Factory<Shape, int> FactoryType;
+  typedef Loki::Factory<Shape, std::string> FactoryType;
   typedef Loki::SingletonHolder<FactoryType, Loki::CreateUsingNew> SingleFactory;
   auto& factory = SingleFactory::Instance();
-  factory.Register(0, (Shape * (*)()) createPolygon);
-  factory.Register(1, (Shape * (*)()) createLine);
-  factory.Register(2, (Shape * (*)()) createCircle);
+  factory.Register("polygon", (Shape * (*)()) createPolygon);
+  factory.Register("line", (Shape * (*)()) createLine);
+  factory.Register("circle", (Shape * (*)()) createCircle);
 
-  auto* a = factory.CreateObject(0);
-  auto* b = factory.CreateObject(1);
-  auto* c = factory.CreateObject(2);
-  EXPECT_EQ(a->calculate(), 3);
+  auto* a1 = factory.CreateObject("polygon");
+  auto* a2 = factory.CreateObject("polygon");
+  a1->setPos({100, 100});
+  a2->setPos({200, 200});
+  auto* b = factory.CreateObject("line");
+  auto* c = factory.CreateObject("circle");
+  EXPECT_EQ(a1->calculate(), 203);
+  EXPECT_EQ(a2->calculate(), 403);
   EXPECT_EQ(b->calculate(), 2);
   EXPECT_EQ(c->calculate(), 360);
+  EXPECT_ANY_THROW(factory.CreateObject("cube"));
 }
