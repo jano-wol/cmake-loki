14b6799fbda6876b58faf65ec291b9ceb3dd97dc
janowol <janos.wolosz@gmail.com>
2024-03-17 21:57:48 +0100

    Add test

--- source/test/abstract_factory/TestAbstractFactory.cpp
+++ source/test/abstract_factory/TestAbstractFactory.cpp
@@ -8,21 +8,21 @@
 class Shape
 {
 public:
-  virtual std::string name() const = 0;
+  virtual std::string getName() const = 0;
   virtual ~Shape(){};
 };
 
 class Shape2D : public Shape
 {
 public:
-  virtual std::string name() const = 0;
+  virtual std::string getName() const = 0;
   virtual ~Shape2D(){};
 };
 
 class Shape3D : public Shape
 {
 public:
-  virtual std::string name() const = 0;
+  virtual std::string getName() const = 0;
   virtual ~Shape3D(){};
 };
 
@@ -30,54 +30,53 @@ class Line : public Shape2D
 {
 public:
   ~Line(){};
-  std::string name() const override { return "Line"; }
+  std::string getName() const override { return "Line"; }
 };
 
 class Circle : public Shape2D
 {
 public:
   ~Circle(){};
-  std::string name() const override { return "Circle"; }
+  std::string getName() const override { return "Circle"; }
 };
 
 class Rectangle : public Shape2D
 {
 public:
   ~Rectangle(){};
-  std::string name() const override { return "Rectangle"; }
+  std::string getName() const override { return "Rectangle"; }
 };
 
 class Plane : public Shape3D
 {
 public:
   ~Plane(){};
-  std::string name() const override { return "Plane"; }
+  std::string getName() const override { return "Plane"; }
 };
 
 class Ball : public Shape3D
 {
 public:
   ~Ball(){};
-  std::string name() const override { return "Ball"; }
+  std::string getName() const override { return "Ball"; }
 };
 
 class Box : public Shape3D
 {
 public:
   ~Box(){};
-  std::string name() const override { return "Box"; }
+  std::string getName() const override { return "Box"; }
 };
 
 typedef Loki::AbstractFactory<TYPELIST_2(Shape2D, Shape3D)> AbstractShapeFactory;
-typedef Loki::ConcreteFactory<AbstractShapeFactory, Loki::OpNewFactoryUnit, TYPELIST_2(Line, Plane)>
-    ShapeFactory2D;
-/* typedef Loki::ConcreteFactory<AbstractShapeFactory, Loki::OpNewFactoryUnit, TYPELIST_3(Plane, Ball, Box)>
-    ShapeFactory3D; */
+typedef Loki::ConcreteFactory<AbstractShapeFactory, Loki::OpNewFactoryUnit, TYPELIST_2(Line, Plane)> LowDimension;
 
 TEST(AbstractFactory, TestAbstractFactory)
 {
-  typedef Loki::SingletonHolder<ShapeFactory2D, Loki::CreateUsingNew> Single2DFactory;
-  auto& factory2D = Single2DFactory::Instance();
-  Shape* pCircle = factory2D.Create<Line>();
-  Shape* pPlane = factory2D.Create<Plane>();
+  typedef Loki::SingletonHolder<LowDimension, Loki::CreateUsingNew> SingleLowDimensionFactory;
+  auto& lowDimensionFactory = SingleLowDimensionFactory::Instance();
+  Shape* pLine = lowDimensionFactory.Create<Shape2D>();
+  Shape* pPlane = lowDimensionFactory.Create<Shape3D>();
+  EXPECT_EQ(pLine->getName(), "Line");
+  EXPECT_EQ(pPlane->getName(), "Plane");
 }
