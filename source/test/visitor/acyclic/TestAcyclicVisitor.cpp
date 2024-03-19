458def8b727a507d83c0eaa7ac2fcf588d59512a
janowol <janos.wolosz@gmail.com>
2024-03-19 07:42:24 +0100

    Add test

--- source/test/visitor/acyclic/TestAcyclicVisitor.cpp
+++ source/test/visitor/acyclic/TestAcyclicVisitor.cpp
@@ -2,6 +2,41 @@
 
 #include <loki/Visitor.h>
 
+class DocElement : public Loki::BaseVisitable<>
+{
+public:
+  DEFINE_VISITABLE()
+};
+
+class Paragraph : public DocElement
+{
+public:
+  DEFINE_VISITABLE()
+};
+
+class MyConcreteVisitor : public Loki::BaseVisitor, public Loki::Visitor<DocElement>, public Loki::Visitor<Paragraph>
+{
+public:
+  MyConcreteVisitor() : docElementCount(0), paragraphCount(0){};
+
+  void Visit(DocElement&) { ++docElementCount; }
+  void Visit(Paragraph&)
+  {
+    ++docElementCount;
+    ++paragraphCount;
+  }
+  int docElementCount;
+  int paragraphCount;
+};
+
 TEST(Visitor, TestAcyclic)
 {
+  MyConcreteVisitor visitor;
+  Paragraph par;
+  DocElement* d1 = &par;
+  DocElement d2;
+  d1->Accept(visitor);
+  d2.Accept(visitor);
+  EXPECT_EQ(visitor.docElementCount, 2);
+  EXPECT_EQ(visitor.paragraphCount, 1);
 }
