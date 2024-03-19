5435908afcc61475f09e1bd6e3f0434e5003cdf8
janowol <janos.wolosz@gmail.com>
2024-03-19 08:47:01 +0100

    Implement acyclic tests

--- source/test/visitor/acyclic/TestAcyclicVisitor.cpp
+++ source/test/visitor/acyclic/TestAcyclicVisitor.cpp
@@ -2,13 +2,25 @@
 
 #include <loki/Visitor.h>
 
-class DocElement : public Loki::BaseVisitable<>
+template <typename R, typename Visited>
+struct DefaultCatchAll
+{
+  static R OnUnknownVisitor(Visited&, Loki::BaseVisitor&) { throw(std::runtime_error("Not registered type!")); }
+};
+
+class DocElement : public Loki::BaseVisitable<void, DefaultCatchAll>
 {
 public:
   DEFINE_VISITABLE()
 };
 
 class Paragraph : public DocElement
+{
+public:
+  DEFINE_VISITABLE()
+};
+
+class Section : public DocElement
 {
 public:
   DEFINE_VISITABLE()
@@ -29,14 +41,31 @@ public:
   int paragraphCount;
 };
 
+class MyStatVisitor : public Loki::BaseVisitor, public Loki::Visitor<DocElement>, public Loki::Visitor<Paragraph>
+{
+public:
+  MyStatVisitor() : docElementCount(0), paragraphCount(0){};
+
+  void Visit(DocElement&) { ++docElementCount; }
+  void Visit(Paragraph&)
+  {
+    ++docElementCount;
+    ++paragraphCount;
+  }
+  int docElementCount;
+  int paragraphCount;
+};
+
 TEST(Visitor, TestAcyclic)
 {
-  MyConcreteVisitor visitor;
+  MyStatVisitor statVisitor;
   Paragraph par;
   DocElement* d1 = &par;
   DocElement d2;
-  d1->Accept(visitor);
-  d2.Accept(visitor);
-  EXPECT_EQ(visitor.docElementCount, 2);
-  EXPECT_EQ(visitor.paragraphCount, 1);
+  d1->Accept(statVisitor);
+  d2.Accept(statVisitor);
+  EXPECT_EQ(statVisitor.docElementCount, 2);
+  EXPECT_EQ(statVisitor.paragraphCount, 1);
+  Section s;
+  EXPECT_ANY_THROW(s.Accept(statVisitor));
 }
