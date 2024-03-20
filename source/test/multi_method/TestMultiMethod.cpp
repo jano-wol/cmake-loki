18d4775cba4fb119f562057a7c99df4b28bbe419
janowol <janos.wolosz@gmail.com>
2024-03-20 11:34:52 +0100

    Add FnDispatcher tests

--- source/test/multi_method/TestMultiMethod.cpp
+++ source/test/multi_method/TestMultiMethod.cpp
@@ -85,13 +85,14 @@ std::string FireRectanglePrinter(Shape& lhs, OutputDevice& rhs)
 
 std::string FireEllipsePrinter(Shape& lhs, OutputDevice& rhs)
 {
-  Ellipse& r = dynamic_cast<Ellipse&>(lhs);
+  Ellipse& e = dynamic_cast<Ellipse&>(lhs);
   Printer& p = dynamic_cast<Printer&>(rhs);
   std::stringstream ss;
-  ss << r.getName() << "-" << p.getName();
+  ss << e.getName() << "-" << p.getName();
   return ss.str();
 }
 
+// FnDispatcher is more convinient than BasicDispatcher
 TEST(MultiMethod, TestBasicDispatcher)
 {
   Loki::BasicDispatcher<Shape, OutputDevice, std::string> disp;
@@ -106,5 +107,48 @@ TEST(MultiMethod, TestBasicDispatcher)
   EXPECT_ANY_THROW(disp.Go(e, *pDev));
 }
 
-typedef Loki::FnDispatcher<Shape> FnDispatcher;
+typedef Loki::FnDispatcher<Shape, OutputDevice, std::string> FnDispatcher;
+typedef Loki::FnDispatcher<Shape, Shape, std::string> FnDispatcherSymmetric;
 
+std::string FireRectanglePrinterSimple(Rectangle& r, Printer& p)
+{
+  std::stringstream ss;
+  ss << r.getName() << "-" << p.getName();
+  return ss.str();
+}
+
+std::string FireEllipsePrinterSimple(Ellipse& e, Printer& p)
+{
+  std::stringstream ss;
+  ss << e.getName() << "-" << p.getName();
+  return ss.str();
+}
+
+std::string FireEllipseRectangleSimple(Ellipse& e, Rectangle& r)
+{
+  std::stringstream ss;
+  ss << e.getName() << "-" << r.getName();
+  return ss.str();
+}
+
+TEST(MultiMethod, TestFnDispatcher)
+{
+  FnDispatcher disp;
+  disp.Add<Rectangle, Printer, FireRectanglePrinterSimple>();
+  Rectangle r;
+  Printer p;
+  Shape* rSh = &r;
+  OutputDevice* pDev = &p;
+  std::string result = disp.Go(*rSh, *pDev);
+  EXPECT_EQ(result, "Rectangle-Printer");
+  Ellipse e;
+  Shape* eSh = &e;
+  EXPECT_ANY_THROW(disp.Go(e, *pDev));
+  FnDispatcherSymmetric dispSymmetric;
+  dispSymmetric.Add<Ellipse, Rectangle, FireEllipseRectangleSimple, true>();
+  result = dispSymmetric.Go(*eSh, *rSh);
+  EXPECT_EQ(result, "Ellipse-Rectangle");
+  result = dispSymmetric.Go(*rSh, *eSh);
+  EXPECT_EQ(result, "Ellipse-Rectangle");
+  EXPECT_ANY_THROW(dispSymmetric.Go(*eSh, *eSh));
+}
