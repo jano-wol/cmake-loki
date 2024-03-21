d5628a297d3680786cc7e5e7b67795a545fd6c1f
Andrzej Polak <jjjkamu@gmail.com>
2024-03-21 17:51:47 +0100

    Tidy up TestThread

--- source/test/thread/TestThread.cpp
+++ source/test/thread/TestThread.cpp
@@ -2,12 +2,7 @@
 
 #include <loki/Singleton.h>
 #include <loki/Threads.h>
-#include <time.h>
-#include <chrono>
-#include <random>
-#include <sstream>
 #include <thread>
-#include <vector>
 
 template <class Host>
 class ClassLevelLockable
@@ -47,6 +42,44 @@ public:
   }
 };
 
+template <class Host>
+class ObjectLevelLockable
+{
+  mutable std::mutex mtx_;
+
+public:
+  class Lock;
+  friend class Lock;
+
+  class Lock
+  {
+    ObjectLevelLockable const& host_;
+    Lock(const Lock&);
+    Lock& operator=(const Lock&);
+
+  public:
+    explicit Lock(const ObjectLevelLockable& host) : host_(host) { host_.mtx_.lock(); }
+    ~Lock() { host_.mtx_.unlock(); }
+  };
+
+  typedef volatile Host VolatileType;
+
+  typedef int IntType;
+
+  static IntType AtomicIncrement(volatile IntType& lval) { return __sync_fetch_and_add(&lval, 1); }
+  static IntType AtomicDecrement(volatile IntType& lval) { return __sync_fetch_and_add(&lval, -1); }
+  static void AtomicAssign(volatile IntType& lval, IntType val)
+  {
+    __sync_lock_test_and_set(&lval, val);
+    __sync_synchronize();
+  }
+  static void AtomicAssign(IntType& lval, volatile IntType& val)
+  {
+    __sync_lock_test_and_set(&lval, val);
+    __sync_synchronize();
+  }
+};
+
 class A
 {
 public:
@@ -55,21 +88,19 @@ public:
   inline static size_t constuctorCount;
 };
 
+// typedef Loki::SingletonHolder<A, Loki::CreateUsingNew, Loki::DefaultLifetime> SingleANew; fails thread sanitizer test
 typedef Loki::SingletonHolder<A, Loki::CreateUsingNew, Loki::DefaultLifetime, ClassLevelLockable> SingleANew;
 
 class TestClass
 {
 public:
-  void operator()(int i, int j) { SingleANew::Instance(); }
-  inline static std::map<int, int> map_;
-  inline static std::mutex mx;
+  void operator()() { SingleANew::Instance(); }
 };
 
-TEST(Thread, TestThread)
+TEST(Thread, TestThreadClassLevelLockableSingleton)
 {
-  srand((unsigned)time(NULL));
-  std::thread t1(TestClass(), 0, 1);
-  std::thread t2(TestClass(), 1, 2);
+  std::thread t1(TestClass{});
+  std::thread t2(TestClass{});
   t1.join();
   t2.join();
   EXPECT_EQ(A::constuctorCount, 1);
